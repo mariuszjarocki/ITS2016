54e47accc4b6217f5d3c200da68390bc409796e1
Piotr Rogulski <piotrr.rgl@gmail.com>
2016-05-26 23:18:21 +0200

    Update user.route.js

--- api1.2/routes/user.route.js
+++ api1.2/routes/user.route.js
@@ -5,7 +5,7 @@ var securityService=require('../services/security.service')
 module.exports=function(router){
     var prefix='/user/';
     router.get(prefix+':id',user.get);
-    router.get('/users/',[securityService.role.isAdmin,user.getUsersList]);
+    router.get('/users/',user.getUsersList);
     router.put(prefix+'update/:id',user.update);
     router.post(prefix+'create',user.create);
     router.post(prefix+'create',user.create);
