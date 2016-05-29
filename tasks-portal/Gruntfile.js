4c851746bf69d772ae2cf17da0710ebee620d7d6
Janusz Bugajny <janusz.bugajny@cybercom.com>
2016-05-29 15:29:25 +0200

    added grunt less and grunt concat tasks.

--- tasks-portal/Gruntfile.js
+++ tasks-portal/Gruntfile.js
@@ -14,13 +14,36 @@ module.exports = function (grunt) {
                     declaration: true
                 }
             }
+        },
+        less: {
+            // production config is also available
+            development: {
+                options: {
+                    // Specifies directories to scan for @import directives when parsing. 
+                    // Default value is the directory of the source, which is probably what you want.
+                    paths: ["app/less/"],
+                },
+                files: {
+                    // compilation.css  :  source.less
+                    "app/less/compiledCss/general.css": "app/less/general.less",
+                    "app/less/compiledCss/tasks.css": "app/less/tasks.less"
+                }
+            },
+        },
+        concat: {
+            dist: {
+                src: ['app/less/compiledCss/*.css'],
+                dest: 'app/app-concatenated.css',
+            },
         }
     });
 
     grunt.loadNpmTasks('grunt-typescript');
-
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     // Default task(s).
-    grunt.registerTask('default', ['typescript']);
+    grunt.registerTask('build', ['typescript', 'less', 'concat'])
+    grunt.registerTask('default', ['build']);
 
 };
